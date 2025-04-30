b32377286b53f895e0858774fbd05a6d495fe9e1
rogerflowey <rogerwangabc@163.com>
2025-04-30 14:34:18 +0800

    feat:a simple rubbish_bin

---
+++ src/disk/rubbish_bin.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+namespace RFlowey {
+  class RubbishBin {
+    std::vector<page_id_t> free_page_;
+  public:
+    RubbishBin() {
+
+    }
+    bool empty() {
+      return free_page_.empty();
+    }
+    void push(page_id_t page_id) {
+      free_page_.push_back(page_id);
+    }
+    page_id_t pop() {
+      auto back = free_page_.back();
+      free_page_.pop_back();
+      return back;
+    }
+  };
+}
